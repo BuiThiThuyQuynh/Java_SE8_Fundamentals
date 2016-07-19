c598a94741a37f8491bb9fd35211dc7defd3e598
thuyquynhbui <thuyquynh.bui.325@gmail.com>
2016-07-19 22:57:19 +0700

    This is my exercise 1.3 in the part Java Basic

---
+++ bai1_3/src/main/bai1_3.java
@@ -0,0 +1,14 @@
+package main;
+
+public class bai1_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("**   **  ******  **      **        ****\n"  );
+		System.out.print("**   **  **      **      **      **    **\n");
+		System.out.print("*******  ******  **      **      **    **\n");
+		System.out.print("**   **  **      **      **      **    **\n");
+		System.out.print("**   **  ******  ******  ******    ****\n");
+	}
+
+}
